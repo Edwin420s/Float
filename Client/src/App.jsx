c8d51e249fa63e4b2e0ed464c85d494593191638
Edwin Mwenda Mwiti <eduedwyn5@gmail.com>
2026-03-03 15:48:28 +0300

    Initial commit

---
+++ Client/src/App.jsx
@@ -0,0 +1,38 @@
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { WalletProvider } from './context/WalletContext'
+import { TreasuryProvider } from './context/TreasuryContext'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+
+// Pages
+import LandingPage from './pages/LandingPage'
+import Dashboard from './pages/Dashboard'
+import Treasury from './pages/Treasury'
+import Transactions from './pages/Transactions'
+import Settings from './pages/Settings'
+import DemoFlow from './pages/DemoFlow'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <WalletProvider>
+        <TreasuryProvider>
+          <div className="min-h-screen bg-primary text-white">
+            <Routes>
+              <Route path="/" element={<LandingPage />} />
+              <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/treasury" element={<Treasury />} />
+              <Route path="/transactions" element={<Transactions />} />
+              <Route path="/settings" element={<Settings />} />
+              <Route path="/demo" element={<DemoFlow />} />
+              <Route path="*" element={<Navigate to="/" />} />
+            </Routes>
+            <ToastContainer position="bottom-right" theme="dark" />
+          </div>
+        </TreasuryProvider>
+      </WalletProvider>
+    </BrowserRouter>
+  )
+}
+
+export default App
