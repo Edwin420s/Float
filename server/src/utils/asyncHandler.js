fc31a74798ab8ea230785bc49e190973d1860966
Edwin Mwenda Mwiti <eduedwyn5@gmail.com>
2026-03-03 18:29:03 +0300

    Initial commit

---
+++ server/src/utils/asyncHandler.js
@@ -0,0 +1,10 @@
+/**
+ * Wraps an async route handler to eliminate try-catch blocks.
+ * @param {Function} fn - Async controller function
+ * @returns {Function} Express middleware
+ */
+const asyncHandler = (fn) => (req, res, next) => {
+  Promise.resolve(fn(req, res, next)).catch(next);
+};
+
+module.exports = asyncHandler;
